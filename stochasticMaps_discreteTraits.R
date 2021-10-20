079d9aba5a63346dca2974a94f02b9960da35281
Joyce Gloria Chery <joycegchery@gmail.com>
2021-10-20 14:10:28 -0400

    Update stochasticMaps_discreteTraits.R

--- stochasticMaps_discreteTraits.R
+++ stochasticMaps_discreteTraits.R
@@ -1,6 +1,5 @@
-
-#function to computer posterior probabilities at the nodes (L40-49) by liam revell from http://blog.phytools.org/2012/12/plotting-node-piecharts-on-top-of.html
-#function to plot simmaps and node posterior probabilities (L74-183) by Dr. Michael May (UC Berkeley)
+#function to computer posterior probabilities at the nodes (L44-52) by liam revell from http://blog.phytools.org/2012/12/plotting-node-piecharts-on-top-of.html
+#function to plot simmaps and node posterior probabilities (L71-180) by Dr. Michael May (UC Berkeley)
 
 library(phytools)
 library(grDevices)
@@ -9,7 +8,7 @@ tree<-read.nexus("Tree.tre")
 
 # upload data 
 data  <- read.csv("DataTable.csv", row.names = 1) 
-datum <- as.data.frame(cbind(rownames(data), data[,13]), row.names = FALSE) #select the column with the trait of interest
+datum <- as.data.frame(cbind(rownames(data), data[,12]), row.names = FALSE) #select the column with the trait of interest
 datum <- datum[complete.cases(datum), ]
 
 # species
@@ -22,22 +21,26 @@ names(datas) <- species
 # drop tips
 pruned_tree <- keep.tip(tree, species)
 
-#DISCRETE TRAITS
-# model fitting
-er  <- fitMk(pruned_tree, datas, model="ER",  pi="estimated")
-ard <- fitMk(pruned_tree, datas, model="ARD",  pi="estimated")
+#POLYMORPHIC TRAITS
+er  <- fitpolyMk(pruned_tree, datas, model="ER",  ordered=TRUE, pi="estimated")
+ard <- fitpolyMk(pruned_tree, datas, model="ARD", ordered=TRUE, pi="estimated")
 
 # chi-square test
-1 - pchisq(2*abs(ard$logLik- er$logLik), 1) # if p is not < .05 then select ER model
+er_num_params  <- 1
+ard_num_params <- sum(ard$index.matrix != 0)
+1 - pchisq(2*abs(ard$logLik- er$logLik), ard_num_params - er_num_params) # if p is not < .05 then selecr ER model
 
-#make simmap
-simmap.trees <- make.simmap(pruned_tree, datas, model = "ARD", pi = "estimated", nsim=1000)
+ard$index.matrix
+ard$data
+
+# stochastic mapping
+simmap.trees <- make.simmap(pruned_tree, ard$data, model = ard$index.matrix, pi = "estimated", nsim=1000)
 
 # make colors
-col_vec <- c("dodgerblue4", "orangered3")
-states<- c("present","absent")
+col_vec <- c("dodgerblue4", "orangered3", "olivedrab4")
+states<- c("hetero","hetero+homo", "homo")
 
-# function to compute the states written by liam revell
+# function to compute the states
 foo<-function(x){
   y<-sapply(x$maps,function(x) names(x)[1])
   names(y)<-x$edge[,1]
@@ -48,7 +51,6 @@ foo<-function(x){
 XX<-sapply(simmap.trees,foo)
 pies<-t(apply(XX,1,function(x,levels,Nsim) summary(factor(x,levels))/Nsim,levels=states,Nsim=1000))
 
-#plot simmaps and posterior node piecharts. See plot_simmap function below at line 74
 plot_simmap(time_tree = simmap.trees[[1]], 
             tree = simmap.trees[[1]], 
             simmaps = simmap.trees, 
@@ -58,18 +60,13 @@ plot_simmap(time_tree = simmap.trees[[1]],
             label.cex = .6,
             colors = col_vec, edge.width=0, nt=10001)
 
-title(main="Axial Parenchyma Length", adj = .0004, line= -.01)
+title(main="Ray Composition", adj = .0004, line= -.01)
 add.simmap.legend(colors=col_vec, prompt=FALSE,x=105,y=180, fsize =.9)
 nodelabels(pie=pies,cex=0.36,piecol=col_vec, lwd=1)
 legend(x = -7, y = 150, legend = states, col = col_vec, pch = 20, yjust = 0, bty = 'n', cex =.7, pt.cex = 4)
 
 
-##add.color.bar(50, col_vec, title="PP(state=1)", lims=c(0,1), digits=1,
-lwd=4, outline=TRUE, x=0,
-y=1-0.08)
-
-
-#####this is the function being use to plot density plot + pie chart             
+#####this is the function being use to plot density plot + pie chart  
 ###written by Mike May 2021
 plot_simmap = function(time_tree, tree, data, simmaps, states, colors, nt=1001, show.tip.label=FALSE, edge.width=0, plot_pie=TRUE, pie_size=1.5, label.offset=0, label.cex=1, lwd=1, ...) {
   ​
