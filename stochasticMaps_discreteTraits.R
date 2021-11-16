8a9b78b1918a768204070be8517a8096ed7a55b4
Joyce Gloria Chery <joycegchery@gmail.com>
2021-11-16 15:23:55 -0500

    Update stochasticMaps_discreteTraits.R

--- stochasticMaps_discreteTraits.R
+++ stochasticMaps_discreteTraits.R
@@ -1,13 +1,14 @@
 #function to computer posterior probabilities at the nodes (L44-52) by liam revell from http://blog.phytools.org/2012/12/plotting-node-piecharts-on-top-of.html
+#function to plot simmaps and node posterior probabilities (L71-180) by Dr. Michael May (UC Berkeley)
 
 library(phytools)
 library(grDevices)
-setwd("~/Desktop/")
+setwd("~/Desktop/SapindalesWoodAnatomy-main/")
 tree<-read.nexus("Tree.tre")
 
 # upload data 
 data  <- read.csv("DataTable.csv", row.names = 1) 
-datum <- as.data.frame(cbind(rownames(data), data[,12]), row.names = FALSE) #select the column with the trait of interest
+datum <- as.data.frame(cbind(rownames(data), data[,2]), row.names = FALSE) #select the column with the trait of interest
 datum <- datum[complete.cases(datum), ]
 
 # species
@@ -20,26 +21,21 @@ names(datas) <- species
 # drop tips
 pruned_tree <- keep.tip(tree, species)
 
-#POLYMORPHIC TRAITS
-er  <- fitpolyMk(pruned_tree, datas, model="ER",  ordered=TRUE, pi="estimated")
-ard <- fitpolyMk(pruned_tree, datas, model="ARD", ordered=TRUE, pi="estimated")
+#DISCRETE TRAITS
+er  <- fitMk(pruned_tree, datas, model="ER",  ordered=TRUE, pi="estimated")
+ard <- fitMk(pruned_tree, datas, model="ARD", ordered=TRUE, pi="estimated")
 
 # chi-square test
-er_num_params  <- 1
-ard_num_params <- sum(ard$index.matrix != 0)
-1 - pchisq(2*abs(ard$logLik- er$logLik), ard_num_params - er_num_params) # if p is not < .05 then selecr ER model
-
-ard$index.matrix
-ard$data
+1 - pchisq(2*abs(ard$logLik- er$logLik), 1) # if p is not < .05 then select ER model
 
 # stochastic mapping
-simmap.trees <- make.simmap(pruned_tree, ard$data, model = ard$index.matrix, pi = "estimated", nsim=1000)
+simmap.trees <- make.simmap(pruned_tree, datas, model = "ER", pi = "estimated", nsim=1000)
 
 # make colors
-col_vec <- c("dodgerblue4", "orangered3", "olivedrab4")
-states<- c("hetero","hetero+homo", "homo")
+col_vec <- c("dodgerblue4", "orangered3")
+states<- c("absent","present") # make colors
 
-# function to compute the states
+# function to compute the node states
 foo<-function(x){
   y<-sapply(x$maps,function(x) names(x)[1])
   names(y)<-x$edge[,1]
@@ -49,6 +45,9 @@ foo<-function(x){
 
 XX<-sapply(simmap.trees,foo)
 pies<-t(apply(XX,1,function(x,levels,Nsim) summary(factor(x,levels))/Nsim,levels=states,Nsim=1000))
+
+#generate summary of stochastic maps with pies of posterior at nodes..
+#see below (line 72) for plot_simmap function
 
 plot_simmap(time_tree = simmap.trees[[1]], 
             tree = simmap.trees[[1]], 
